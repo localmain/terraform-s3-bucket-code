71639c65fc87c35d268005bb73ea898e54ca6080
localmain <abhi143mohanty@gmail.com>
2022-11-20 12:36:33 +0530

    new commit

--- main.tf
+++ main.tf
@@ -13,14 +13,5 @@ resource "aws_s3_bucket" "terraform_state_s3" {
   versioning {
          enabled = true
         }
-
-# Enable server-side encryption by default
-server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
